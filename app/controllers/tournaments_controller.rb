78a8229e1022d00166dd44759930a7ec3c232f26
Michael Tung <tung@hotmail.it>
2020-11-03 18:04:21 +0100

    made adjustments to controller and changed to simple form for views

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,10 +1,16 @@
 class TournamentsController < ApplicationController
+
+  before_action :find_tournament, only: [:show, :edit,:destroy]
+
   def index
     @tournaments = Tournament.all
   end
 
   def show
-    @tournaments = Tournament.find(params[:id])
+  end
+
+  def show_user_tournaments
+    @tournaments = Tournament.where(user_id: current_user.id)
   end
 
   def new
@@ -13,30 +19,37 @@ class TournamentsController < ApplicationController
 
   def create
     @tournament = Tournament.new(tournament_params)
-    @tournament.save
-
-    redirect_to tournament_path(@tournament)
+    @tournament.user = current_user
+    if @tournament.save
+      redirect_to tournament_path(@tournament)
+    else
+      render 'new'
+    end
   end
 
   def edit
-    @tournament = Tournament.find(params[:id])
   end
 
   def update
     @tournament = Tournament.find(tournament_params)
-    @tournament.update(params[:tournament])
-
-    redirect_to tournament_path(@tournament)
+    if @tournament.update(params[:tournament])
+      redirect_to tournament_path(@tournament), notice: 'Tournament successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
-    @tournament = Tournament.find(params[:id])
     @tournament.destroy
 
     redirect_to tournament_path
   end
 
   private
+
+  def find_tournament
+    @tournament = Tournament.find(params[:id])
+  end
 
   def tournament_params
     params.require(:tournament).permit(:name, :description, :location, :number_of_teams, :number_of_players_per_team, :price, :start_time, :end_time)
