1d41dab0cd95283dc1685ad4870ca7dbcc0cf879
Giacomo Bonomelli <gbonomelli483@gmail.com>
2020-11-03 20:25:21 +0100

    Changed Tables of db

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ Sport.create(name: 'Tennis')
 Structure.create(name: 'Type 1')
 
 #Optional Subscription Field Seeds
-Optional.create(role: false, team: true, player_number: false)
+#Optional.create(role: false, team: true, player_number: false)
 
 #User Seeds
 User.create(first_name: "Yassine", last_name: "Chaqroun", email: "yc@email.com", password: "123456", address: "Casablanca, Morocco")
@@ -48,11 +48,11 @@ JoinTeamPlayer.create(user_id: 3, team_id: 3)
 JoinTeamPlayer.create(user_id: 4, team_id: 3)
 
 #Tournament Seeds
-Tournament.create(name: "Champions League", location: "Madrid, Spain", description: "Great tournament!", number_of_teams: 5, number_of_players_per_team: 5, price: 1000, sport_id: 1, user_id: 1, structure_id: 1, optional_id: 1)
-Tournament.create(name: "Europa League", location: "Milan, Italy", description: "Good tournament!", number_of_teams: 4, number_of_players_per_team: 2, price: 800, sport_id: 1, user_id: 2, structure_id: 1, optional_id: 1)
-Tournament.create(name: "Players League", location: "Paris, France", description: "Nice tournament!", number_of_teams: 3, number_of_players_per_team: 3, price: 700, sport_id: 1, user_id: 3, structure_id: 1, optional_id: 1)
-Tournament.create(name: "Cool League", location: "London, UK", description: "Cool tournament!", number_of_teams: 2, number_of_players_per_team: 5, price: 600, sport_id: 1, user_id: 4, structure_id: 1, optional_id: 1)
-Tournament.create(name: "Pro League", location: "Berlin, Germany", description: "Well-organized tournament!", number_of_teams: 4, number_of_players_per_team: 1, price: 500, sport_id: 1, user_id: 1, structure_id: 1, optional_id: 1)
+Tournament.create(name: "Champions League", location: "Madrid, Spain", description: "Great tournament!", number_of_teams: 5, number_of_players_per_team: 5, price: 1000, sport_id: 1, user_id: 1, structure_id: 1)
+Tournament.create(name: "Europa League", location: "Milan, Italy", description: "Good tournament!", number_of_teams: 4, number_of_players_per_team: 2, price: 800, sport_id: 1, user_id: 2, structure_id: 1)
+Tournament.create(name: "Players League", location: "Paris, France", description: "Nice tournament!", number_of_teams: 3, number_of_players_per_team: 3, price: 700, sport_id: 1, user_id: 3, structure_id: 1)
+Tournament.create(name: "Cool League", location: "London, UK", description: "Cool tournament!", number_of_teams: 2, number_of_players_per_team: 5, price: 600, sport_id: 1, user_id: 4, structure_id: 1)
+Tournament.create(name: "Pro League", location: "Berlin, Germany", description: "Well-organized tournament!", number_of_teams: 4, number_of_players_per_team: 1, price: 500, sport_id: 1, user_id: 1, structure_id: 1)
 
 #Booking Seeds
 Booking.create(user_id: 1, team_id: 1, tournament_id: 1)
