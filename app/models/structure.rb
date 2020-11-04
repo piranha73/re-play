2d0d2eb4c72d578f208412804381b83ad551b2ac
Michele Comolli <mikicomolli@gmail.com>
2020-11-04 22:21:08 +0100

    new method games_generator(tournament) in stucture model

--- app/models/structure.rb
+++ app/models/structure.rb
@@ -1,3 +1,32 @@
 class Structure < ApplicationRecord
   has_many :tournaments
+
+  def games_generator(tournament)
+    # teams = ["team1","team2","team3","team4","team5"]
+    teams = tournament.teams.pluck(:id)
+    matchdays = RoundRobinTournament.schedule(teams)
+    matchdays.each_with_index do |matchday, index|
+      day = Matchday.create(
+        number: index + 1,
+        tournament: tournament
+      )
+      matchday.each_with_index do |game, index|
+        if teams.length.odd?
+          next if index == 0
+
+          Game.create(
+            home_team_id: game.first,
+            away_team_id: game.last,
+            matchday: day
+          )
+        else
+          Game.create(
+            home_team_id: game.first,
+            away_team_id: game.last,
+            matchday: day
+          )
+        end
+      end
+    end
+  end
 end
