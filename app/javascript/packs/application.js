f72953679e544728fa3bda233c44ac981f9d8564
Michael Tung <tung@hotmail.it>
2020-11-06 15:12:35 +0100

    first draft of personal page and general changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -24,11 +25,12 @@ require("channels")
 
 // External imports
 import "bootstrap";
-
+import {tabsFunction} from "../components/tabs";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  tabsFunction();
 });
