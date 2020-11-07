028c633a5d5482d14e30dc8c32a6f507d7341950
Michael Tung <tung@hotmail.it>
2020-11-07 13:56:14 +0100

    tournament show

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,6 +26,7 @@ require("channels")
 // External imports
 import "bootstrap";
 import {tabsFunction} from "../components/tabs";
+import {tabToggle} from "../components/tabs2";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
@@ -33,4 +34,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   tabsFunction();
+  tabToggle();
 });
