e4e93abe14355fbeff9d0bf995102ba66f8e6865
Michele Comolli <mikicomolli@gmail.com>
2020-11-10 00:44:59 +0100

    Added Method to calculate ranking of a tourenament

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -8,4 +8,21 @@ class Tournament < ApplicationRecord
   has_many :games, through: :matchdays
   geocoded_by :location
   after_validation :geocode
+
+  def score(team)
+    score = 0
+    games.where(home_team: team).or(games.where(away_team: team)).each do |game|
+      score += game.score(team) if game.is_started?
+    end
+    return score
+  end
+
+  def ranking
+    ranking = []
+    teams = self.teams.uniq
+    teams.each do |team|
+      ranking << [team, score(team)]
+    end
+    return ranking.sort_by { |array| -array[1] }
+  end
 end
