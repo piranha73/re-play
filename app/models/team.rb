a8981d7a238dc290e9abb815eb2ba64b285279e8
Michele Comolli <mikicomolli@gmail.com>
2020-11-09 14:20:39 +0100

    changed logic in matchday, structure, team Model

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,10 +5,12 @@ class Team < ApplicationRecord
   has_many :users, through: :join_team_players
 
   def players_in_tournament(tournament)
-    tournament.bookings.where(team: self).size
+    tournament.bookings.where(team: self).map do |booking|
+      booking.user
+    end
   end
 
   def enough_players?(tournament)
-    players_in_tournament(tournament) >= tournament.number_of_players_per_team
+    players_in_tournament(tournament).size >= tournament.number_of_players_per_team
   end
 end
