8f5535e26cec899c41d428faa05c870dbea8f2c5
Michele Comolli <mikicomolli@gmail.com>
2020-11-12 13:17:38 +0100

    added start and finish buttons to game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,36 @@
 class GamesController < ApplicationController
+
+  before_action :set_game
+
   def show
     @tournament = Tournament.find(params[:tournament_id])
-    @game = Game.find(params[:id])
     @home_team = @game.home_team
     @away_team = @game.away_team
-    @statistic = Statistic.new
+    if @game.start_time.nil?
+      render 'games/game_not_started'
+    elsif @game.start_time && @game.end_time.nil?
+      @statistic = Statistic.new
+      render 'games/show'
+    else
+      render 'games/game_ended'
+    end
+  end
+
+  def start
+    @tournament = @game.matchday.tournament
+    @game.update(start_time: DateTime.now)
+    redirect_to tournament_game_path(@tournament, @game)
+  end
+
+  def finish
+    @tournament = @game.matchday.tournament
+    @game.update(end_time: DateTime.now)
+    redirect_to tournament_game_path(@tournament)
+  end
+
+  private
+
+  def set_game
+    @game = Game.find(params[:id])
   end
 end
